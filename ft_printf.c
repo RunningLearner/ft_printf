2a78c1b16f7b5b2b6fe085de7863a34bef8037da
seunam <seunam@student.42seoul.kr>
2022-04-19 17:32:01 +0900

    parse_3rd

--- ft_printf.c
+++ ft_printf.c
@@ -6,36 +6,26 @@
 /*   By: seunam <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/17 18:32:21 by seunam            #+#    #+#             */
-/*   Updated: 2022/04/18 16:45:33 by seunam           ###   ########.fr       */
+/*   Updated: 2022/04/19 17:31:55 by seunam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_printf.h"
 
-static int	parising(const char *type, va_list ap)
+static int	parising(char type, va_list ap)
 {
 	int	printcnt;
 	
 	printcnt = 0;
-	while (*type)
-	{
-		if (*type == '%')
-		{	
-			type ++;
-			if (*type == 'c' || *type == '%')
-				printcnt += printchar(va_arg(ap, int)); 
-			if (*type == 's')
-				printcnt += printstr(va_arg(ap, char *)); 
-			if (*type == 'p')
-				printcnt += printptr(va_arg(ap, long long)); 
-			if (*type == 'i' || *type == 'd')
-				printcnt += printint(va_arg(ap, int)); 
-			if (*type == 'u' || *type == 'x' || *type == 'X')
-				printcnt += printuint(va_arg(ap, unsigned int)); 
-		}
-		else
-			printcnt += write(1, type, 1);
-		type ++;
-	}
+	if (type == 'c' || type == '%')
+		printcnt = printchar(va_arg(ap, int)); 
+	if (type == 's')
+		printcnt = printstr(va_arg(ap, char *)); 
+	if (type == 'p')
+		printcnt = printptr(va_arg(ap, unsigned long long)); 
+	if (type == 'i' || type == 'd')
+		printcnt = printint(va_arg(ap, int)); 
+	if (type == 'u' || type == 'x' || type == 'X')
+		printcnt = printuint(va_arg(ap, unsigned int), type); 
 	return (printcnt);
 }
 
@@ -45,7 +35,17 @@ int	ft_printf(const char *type, ...)
 	int	printcnt;
 
 	va_start(ap, type);
-	printcnt = parsing(type, ap);
+	while (*type)
+	{
+		if (*type == '%' && ft_strchr("cspduix", *(type + 1)))
+		{
+			printcnt += parsing(*(type + 1), ap);
+			type ++;
+		}
+		else
+			printcnt += write (1, type, 1);
+		type ++;
+	}
 	va_end(ap);
 	return (prnitcnt);
 }
