837e3d47cfc4c43ed30dfc536f9dba7a0af671ef
seunam <seunam@student.42seoul.kr>
2022-04-19 17:31:40 +0900

    parse_3rd

--- ft_parsing.c
+++ ft_parsing.c
@@ -6,7 +6,7 @@
 /*   By: seunam <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 16:22:37 by seunam            #+#    #+#             */
-/*   Updated: 2022/04/18 16:58:39 by seunam           ###   ########.fr       */
+/*   Updated: 2022/04/19 17:31:26 by seunam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,26 +36,56 @@ static int	printstr(char *arg)
 	return (cnt);
 }
 
-static int	printptr(long long arg)
+static int	printptr(unsigned long long arg)
 {
 	int	cnt;
 
 	cnt = 0;
+	cnt += write(1, "0x", 2);
+	while (arg)
+	{
+		print
+		cnt += write(1, "0123456789abcdef"[arg % 16],1);
+	}
 	return (cnt);
 }
 
 static int	printint(int arg)
 {
 	int	cnt;
+	char	*str;
 
 	cnt = 0;
+	str = itoa(arg);
+	cnt = strlen(str);
+	while (*str)
+	{
+		write(1, str, 1);
+		str ++;
+	}
 	return (cnt);
 }
 
-static int	printuint(unsigned int arg)
+static int	printuint(unsigned int arg, char type)
 {	
 	int	cnt;
+	char	str[10];
 
 	cnt = 0;
+	if (arg == 0)
+	{
+		write(1, "0", 1);
+		return (1);
+	}
+	while (arg)
+	{
+		str[cnt] = (arg % 10) + '0';
+		arg /= 10;
+		cnt ++;
+	}
+	while (str >= 0)
+	{
+		write(1, str--, 1);
+	}
 	return (cnt);
 }
